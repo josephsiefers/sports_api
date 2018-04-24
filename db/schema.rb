dd1ceafe5e52da778e13e62998227bcbdc1b42ec
Joseph Siefers <joseph.siefers@gmail.com>
2018-04-24 17:26:12 -0400

    Remove name brief column from player model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424194417) do
+ActiveRecord::Schema.define(version: 20180424212215) do
 
   create_table "players", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "name_brief"
     t.string "first_name"
     t.string "last_name"
     t.string "position"
